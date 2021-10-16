637e2512f71df8cd87eace99b3d2875b67b7a5a7
Amrit Raj <77192710+amritmaurya1504@users.noreply.github.com>
2021-10-16 09:40:34 +0530

    new changes

--- routes/memeberShipRoute.js
+++ routes/memeberShipRoute.js
@@ -7,6 +7,6 @@ const { isUser } = require("../middlewares/isUser")
 router.route('/getproducts').get(getProducts);
 router.route("/razorpay/:productId").get(membership)
 router.route("/membershipVerification").post(membershipVerification)
-router.route("/getmembershipdetails").get(isUser , verifyPayment)
+router.route("/getmembershipdetails").post(isUser , verifyPayment)
 
 module.exports = router;

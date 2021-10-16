e6dbec052bfcc0a299c05326b9e0b37838a06013
Amrit Raj <77192710+amritmaurya1504@users.noreply.github.com>
2021-10-16 10:07:06 +0530

    new changes

--- controllers/memberShip.js
+++ controllers/memberShip.js
@@ -90,13 +90,13 @@ const verifyPayment = async (req, res) => {
     try {
         const { razorpay_payment_id, razorpay_order_id, razorpay_signature } = req.body;
 
-        generated_signature = hmac_sha256(razorpay_order_id + "|" + razorpay_payment_id, "Cz9OJthuLh983tCKj5moXkxI");
+        const generated_signature = hmac_sha256(razorpay_order_id + "|" + razorpay_payment_id, "Cz9OJthuLh983tCKj5moXkxI");
         if (generated_signature == razorpay_signature) {
             const data = new PayStats({ user: req.user._id, razorpay_payment_id, razorpay_order_id, razorpay_signature })
             await data.save();
-            res.json({ message: "payment is successful " })
+            res.status(200).json({ message: "payment is successful" })
         } else {
-            res.json({ message: "Payment Failed!" })
+            res.status(404).json({ error: "Payment Failed!" })
         }
 
     } catch (error) {

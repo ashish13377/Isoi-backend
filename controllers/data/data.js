5b622835dd24cca564891cf764c12465f94430fc
ashish13377 <52818192+ashish13377@users.noreply.github.com>
2021-10-17 10:43:58 +0530

    update

--- controllers/data/data.js
+++ controllers/data/data.js
@@ -2,14 +2,14 @@ exports.products = [
     {
         id: 1,
         name: "1 Year Membership",
-        desc: "In this membership, you will get a 1-year membership of the ISOI student chapter of HITK and you will get access to membership events and also get all benefits of the ISOI Student chapter HITK.",
-        price: "100",
+        desc: "1-year membership plan of the ISOI student chapter, Free access to certified membership events and premium access to learning resources.",
+        price: "125",
         currency: "INR"
     },
     {
         id: 2,
         name: "4 Year Membership",
-        desc: "In this membership, you will get a 4-years membership of the ISOI student chapter of HITK and you will get access to membership events and also get all benefits of the ISOI Student chapter HITK..",
+        desc: "4-year membership plan of the ISOI student chapter, Free access to certified membership events and premium access to learning resources.",
         price: "400",
         currency: "INR"
     }

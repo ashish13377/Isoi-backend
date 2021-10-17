00312de59337c80ba71cbc0bc30423d69f2759ef
Amrit Raj <77192710+amritmaurya1504@users.noreply.github.com>
2021-10-18 00:55:54 +0530

    new changes

--- controllers/memberShip.js
+++ controllers/memberShip.js
@@ -99,7 +99,7 @@ const verifyPayment = async (req, res) => {
 
     try {
         const { razorpay_payment_id, razorpay_order_id, razorpay_signature } = req.body;
-        const key_secret = process.env.RZP_KEY;
+        const key_secret = process.env.RZP_KEY_SECRET;
 
         const generated_signature = hmac_sha256(razorpay_order_id + "|" + razorpay_payment_id, key_secret).toString();
         if (generated_signature === razorpay_signature) {
